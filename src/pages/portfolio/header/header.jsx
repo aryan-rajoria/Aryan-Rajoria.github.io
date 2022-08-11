11e72b042ed61f5355c546d9f630893d5aa5a1b3
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2022-08-11 21:27:44 +0530

    adding header css

--- src/pages/portfolio/header/header.jsx
+++ src/pages/portfolio/header/header.jsx
@@ -3,20 +3,23 @@ import React from "react";
 
 function header() {
     return (
-        <div classname="header">
-            <br />
-            <p>Hi I am</p>
-            <h1>Aryan Rajoria</h1>
-            <h2>Programmer, AI engineer, Web developer</h2>
-            <h3>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </h3>
+        <div className="header">
+            <div className="header_content">
+                <br />
+                <p>Hi I am</p>
+                <h1>Aryan Rajoria</h1>
+                <h2>Programmer, AI engineer, Web developer</h2>
+                <h3>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </h3>
+            </div>
         </div>
     );
 }
