0c85d2efb47c59bf405fa9421d992432cfbe538c
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2022-08-14 18:38:29 +0530

    content update

--- src/pages/portfolio/aboutme/aboutme.jsx
+++ src/pages/portfolio/aboutme/aboutme.jsx
@@ -11,10 +11,10 @@ function aboutme() {
                 interest in the field of Machine Learning and Artificial
                 Intelligence. Using my strong background in software development
                 and experience of working on a wide variety of projects, I would
-                to pursue a career in ML research and a good internship would
-                consolidate my future career choice. I’m personally interested
-                in the field of meta learning and it use to improve the
-                performance of machine learning algorithms.
+                like to pursue a career in ML research and a good internship
+                would consolidate my future career choice. I’m personally
+                interested in the field of meta learning and its use to improve
+                the performance of machine learning algorithms.
             </p>
         </div>
     );
